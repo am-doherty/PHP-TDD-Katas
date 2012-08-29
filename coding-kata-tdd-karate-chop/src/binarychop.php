695947f17bdaf9c1aab9fe6af24b586a5f9e5355
am-doherty <me@anthonydoherty.co.uk>
2012-08-29 11:59:16 +0100

    Refactored slice mechanism into private function, created private variables for needle and haystack, assigned if the initial arguments are of correct type and length.

--- coding-kata-tdd-karate-chop/src/binarychop.php
+++ coding-kata-tdd-karate-chop/src/binarychop.php
@@ -1,21 +1,31 @@
 <?php
 class Binarychop {
-	function chop($i,$a){
+	private $needleInteger;
+	private $haystackArray;
+	function chop($inputInt,$inputArray){
 		// Response for insufficient/incorrect req's
-		if (!is_array($a) || count($a) < 1 || !is_int($i)) {
+		if (!is_array($inputArray) || count($inputArray) < 1 || !is_int($inputInt)) {
 			return -1;
 		}
+		$a = $inputArray;
+		$i = $inputInt;
+		$this->needleInteger = $inputInt;
+		$this->haystackArray = $inputArray;
 		// Quickie response for single item arrays
-		if (count($a) ===1) {
-			return ($i === $a[0] ) ? 0 : -1;
+		if (count($this->haystackArray) ===1) {
+			return ($this->needleInteger === $this->haystackArray[0] ) ? 0 : -1;
 		}
 		// Handle dual item arrays
-		if (count($a) ===2) {
-			if ($i === $a[0]) return 0;
-			if ($i === $a[1]) return 1;
+		if (count($this->haystackArray) ===2) {
+			if ($this->needleInteger === $this->haystackArray[0]) return 0;
+			if ($this->needleInteger === $this->haystackArray[1]) return 1;
 			return -1;
 		}
-		/*Response for multi-item arrays:
+		return $this->haystackSlice();
+		
+ 	}	
+	private function haystackSlice() {
+			/*Response for multi-item arrays:
 		  *
 		  *    Find middle element, rounding down, assign key and value to $midKey and $midElem respectively
 		  *  	If the needle matches the middle item (you never know) then return key		  
@@ -24,23 +34,25 @@ class Binarychop {
 		  *   	If needle is greater than middle item, derive upper half array and check for needle
 		  *	Finally, if needle still not found, return -1
 		  */
-		$midElem = $a[$midKey = (floor(count($a)/2))];
-		if ($i === $midElem) return $midKey; 
-		$one = array_slice($a,0,$midKey);
+		$midElem = $this->haystackArray[$midKey = (floor(count($this->haystackArray)/2))];
+		if ($this->needleInteger === $midElem) return $midKey; 
+		$one = array_slice($this->haystackArray,0,$midKey);
 
- 		if ($i < $midElem){
-			if (($key = array_search($i,$one)) !==FALSE) {			 
+ 		if ($this->needleInteger < $midElem){
+			if (($key = array_search($this->needleInteger,$one)) !==FALSE) {			 
 				return $key;
 			}
 		} 
 		else	{ 
-		 	$two = array_slice($a,$midKey);
-			if (($key = array_search($i,$two))!==FALSE) {
+		 	$two = array_slice($this->haystackArray,$midKey);
+			if (($key = array_search($this->needleInteger,$two))!==FALSE) {
 			 	return ($key+count($one)); 
 			}
 		} 
 		//finally, return -1 if not found at all
 		return -1;
- 	}	
+	}
+	
+	
 }
 ?>
