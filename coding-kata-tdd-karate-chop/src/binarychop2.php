bc405fbd1cf2a619f85b427e10808708502f9ea8
am-doherty <me@anthonydoherty.co.uk>
2012-08-30 13:02:55 +0100

    Added second attempt, using while loop to iterate reduction of haystack and report final outcome. Uses unset to reduce size of array to zero when a match is found. First four of previous tests sucessfully passed.

---
+++ coding-kata-tdd-karate-chop/src/binarychop2.php
@@ -0,0 +1,42 @@
+<?php
+class Binarychop2 {
+	private $sliceDisgardCount = 0;
+	private $needleInteger;
+	private $haystackArray;
+	private $indexFound = -1;	
+		
+	function chop($inputInt,$inputArray){
+		// Response for insufficient/incorrect req's
+		if (!is_array($inputArray) || count($inputArray) < 1 || !is_int($inputInt)) {
+			return -1;
+		}
+		$this->needleInteger = $inputInt;
+		$this->haystackArray = $inputArray;
+		while(count($this->haystackArray) > 0) {
+			$this->reduceHayStack();
+		}
+		return ($this->indexFound+$this->sliceDisgardCount);
+ 	}	
+
+	private function reduceHayStack( ) {
+		if (count($this->haystackArray) === 1) {
+			$this->indexFound = ($this->needleInteger === $this->haystackArray[0])?0: -1;
+			unset($this->haystackArray[0]);
+			return true;
+		}
+		$midElem = $this->haystackArray[$midKey = (floor(count($this->haystackArray)/2))];
+		if ($this->needleInteger === $midElem) {
+			$this->indexFound = $midKey;
+			return true;
+		}
+		elseif ($this->needleInteger < $midElem){
+			$this->haystackArray = array_slice($this->haystackArray,0,$midKey); 
+ 		} 
+		else {
+			 $this->sliceDisgardCount +=$midKey;
+			 $this->haystackArray = array_slice($this->haystackArray,$midKey); 
+ 		}
+		return true;
+	}
+}
+?>
---
+++ coding-kata-tdd-karate-chop/tests/karate-chop-tests-2.php
@@ -0,0 +1,92 @@
+<?php
+// see http://codekata.pragprog.com/2007/01/kata_two_karate.html
+require_once '../src/binarychop2.php';
+require_once 'PHPUnit/Autoload.php';
+
+class chopTest extends PHPUnit_Framework_TestCase
+{
+	/* Search for integer '3' in empty array */
+    public function testChop()
+    {
+		$myChopper = new Binarychop2();
+        $this->assertEquals(-1, $myChopper->chop(3,array()));
+    }
+	/* Search for integer '3' in array(1) */
+    public function testChop2()
+    {
+		$myChopper = new Binarychop2();
+		$this->assertEquals(-1, $myChopper->chop(3,array(1)));
+    }	
+	/* Search for integer '1' in array(1) */
+    public function testChop3()
+    {
+		$myChopper = new Binarychop2();
+		$this->assertEquals(0, $myChopper->chop(1,array(1)));
+    }	
+	/* Search for integer '1' in array(1,3,5) */
+    public function testChop4()
+    {
+		$myChopper = new Binarychop2();
+		$this->assertEquals(0, $myChopper->chop(1,array(1, 3, 5)));
+    }	
+	// '/* Search for integer '3' in array(1,3,5) */
+    // public function testChop5()
+    // {
+		// $myChopper = new Binarychop2();
+		// $this->assertEquals(1, $myChopper->chop(3,array(1, 3, 5)));
+    // }
+	// /* Search for integer '5' in array(1,3,5) */
+    // public function testChop6()
+    // {
+		// $myChopper = new Binarychop2();
+		// $this->assertEquals(2, $myChopper->chop(5,array(1, 3, 5)));
+    // }	
+	// /* test Incorrect argument types ---- Search for string 'foo' in array(1,3,5) */
+    // public function testChop7()
+    // {
+		// $myChopper = new Binarychop2();
+		// $this->assertEquals(-1, $myChopper->chop('foo',array(1, 3, 5)));
+    // }
+	// /* test Incorrect argument types ---- Search for integer 5 in string */
+    // public function testChop8()
+    // {
+		// $myChopper = new Binarychop2();
+		// $this->assertEquals(-1, $myChopper->chop(5,'a string'));
+    // }		
+	// /* Search for integer '5' in array(1,3,5) */
+    // public function testChop9()
+    // {
+		// $myChopper = new Binarychop2();
+		// $this->assertEquals(2, $myChopper->chop(5,array(1, 3, 5,7,9)));
+    // }	
+	// /* Search for integer '12' in longer array */
+    // public function testChop10()
+    // {
+		// $myChopper = new Binarychop2();
+		// $this->assertEquals(2, $myChopper->chop(5,array(1, 3, 5,7,9,12,34,123,223,278,450,1124,1900)));
+    // }	
+	// /* Search for integer '450' in longer array */
+    // public function testChop11()
+    // {
+		// $myChopper = new Binarychop2();
+		// $this->assertEquals(10, $myChopper->chop(450,array(1, 3, 5,7,9,12,34,123,223,278,450,1124,1900)));
+    // }		
+	// /* Search for integer '450' in longer array (doesnt exist) */
+    // public function testChop12()
+    // {
+		// $myChopper = new Binarychop2();
+		// $this->assertEquals(-1, $myChopper->chop(221,array(1, 3, 5,7,9,12,34,123,223,278,450,1124,1900)));
+    // }	
+	// /* Search for integer '450' in longer array, of even number of items */
+    // public function testChop13()
+    // {
+		// $myChopper = new Binarychop2();
+		// $this->assertEquals(10, $myChopper->chop(450,array(1, 3, 5,7,9,12,34,123,223,278,450,1124)));
+    // }		
+    // public function testChop14()
+    // {
+		// $myChopper = new Binarychop2();
+		// $this->assertEquals(13, $myChopper->chop(450,array(1, 3, 5,5,7,9,12,12,34,123,223,278,278,450,1124)));
+    // }		
+}
+?>
